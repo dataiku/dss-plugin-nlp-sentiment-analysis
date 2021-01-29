26c3245f4e637e0a040e5c2cd776aa0f275ad103
Marsobad <marine.sobas@dataiku.com>
2021-01-29 15:20:35 +0100

    remove the unecessray client parameters

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -4,25 +4,20 @@ project_key = "PLUGINTESTSENTIMENTANALYSIS"
 
 
 def test_binary_classification(user_dss_clients):
-    dss_scenario.run(client=user_dss_clients, project_key=project_key, scenario_id="TESTBINARYCLASSIFICATION",
-                     user="user1")
+    dss_scenario.run(client=user_dss_clients, project_key=project_key, scenario_id="TESTBINARYCLASSIFICATION")
 
 
 def test_5_points_scale_classification(user_dss_clients):
-    dss_scenario.run(client=user_dss_clients, project_key=project_key, scenario_id="TEST5POINTSSCALECLASSIFICATION",
-                     user="default")
+    dss_scenario.run(client=user_dss_clients, project_key=project_key, scenario_id="TEST5POINTSSCALECLASSIFICATION")
 
 
 def test_different_settings(user_dss_clients):
-    dss_scenario.run(client=user_dss_clients, project_key=project_key, scenario_id="Test_different_options",
-                     user="default")
+    dss_scenario.run(client=user_dss_clients, project_key=project_key, scenario_id="Test_different_options")
 
 
 def test_edge_cases(user_dss_clients):
-    dss_scenario.run(client=user_dss_clients, project_key=project_key, scenario_id="Test_edge_cases",
-                     user="default")
+    dss_scenario.run(client=user_dss_clients, project_key=project_key, scenario_id="Test_edge_cases")
 
 
 def test_partition_by_language(user_dss_clients):
-    dss_scenario.run(client=user_dss_clients, project_key=project_key, scenario_id="Test_partition_by_language",
-                     user="default")
+    dss_scenario.run(client=user_dss_clients, project_key=project_key, scenario_id="Test_partition_by_language")
