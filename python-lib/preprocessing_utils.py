42579dad51757911ce50431488661a95184b9a69
admin <alexandre.combessie@dataiku.com>
2021-04-12 13:08:25 +0200

    start python2+3 refactoring

--- python-lib/preprocessing_utils.py
+++ python-lib/preprocessing_utils.py
@@ -1,28 +1,24 @@
 # -*- coding: utf-8 -*-
 import re
 import string
-maketrans = string.maketrans
+import six
 
 
 # Twitter related tokens
-RE_HASHTAG = ur'#[a-zA-Z0-9_]+'
-RE_MENTION = ur'@[a-zA-Z0-9_]+'
+RE_HASHTAG = u"#[a-zA-Z0-9_]+"
+RE_MENTION = u"@[a-zA-Z0-9_]+"
 
-RE_URL = ur'(?:https?://|www\.)(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-RE_EMAIL = ur'\b[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+\b'
+RE_URL = r"(?:https?://|www\.)(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+RE_EMAIL = r"\b[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+\b"
 
-TOKENS_TO_IGNORE = [
-    RE_HASHTAG,
-    RE_MENTION,
-    RE_URL,
-    RE_EMAIL
-]
+
+TOKENS_TO_IGNORE = [RE_HASHTAG, RE_MENTION, RE_URL, RE_EMAIL]
 
 
 def clean_text(text):
     """
     Applies some pre-processing to clean text data.
-    
+
     In particular:
     - lowers the string
     - removes URLs, e-mail adresses
@@ -37,27 +33,29 @@ def clean_text(text):
 
     # ignore urls, mails, twitter mentions and hashtags
     for regex in TOKENS_TO_IGNORE:
-        text = re.sub(regex, ' ', text)
+        text = re.sub(regex, " ", text)
 
-    text = re.sub(r'<[^>]*>', ' ', text)  # remove HTML tags if any
+    text = re.sub(r"<[^>]*>", " ", text)  # remove HTML tags if any
 
     # remove the character [']
-    text = re.sub(r"\'", "", text)    
+    text = re.sub(r"\'", "", text)
 
     # this is the default cleaning in Keras,
     # it consists in lowering the texts and removing the punctuation
-    filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n'
+    filters = '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n'
 
     split = " "  # character that will be used to split the texts later
 
-    if isinstance(text, unicode):
-        translate_map = dict((ord(c), unicode(split)) for c in filters)
+    if isinstance(text, six.text_type):
+        translate_map = dict((ord(c), six.u(split)) for c in filters)
         text = text.translate(translate_map)
     elif len(split) == 1:
-        translate_map = maketrans(filters, split * len(filters))
+        translate_map = (
+            string.maketrans(filters, split * len(filters)) if six.PY2 else str.maketrans(filters, split * len(filters))
+        )
         text = text.translate(translate_map)
     else:
         for c in filters:
             text = text.replace(c, split)
-        
+
     return text
---
+++ tests/python/unit/test_clean_text.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# This is a test file intended to be used with pytest
+# pytest automatically runs all the function starting with "test_"
+# see https://docs.pytest.org for more information
+
+from preprocessing_utils import clean_text
+
+
+def test_dummy_function():
+    assert True
