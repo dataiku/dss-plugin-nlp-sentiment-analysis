2e26891d8f00349b2fc7028afd7f07df2d1b64a4
admin <alexandre.combessie@dataiku.com>
2021-04-12 16:40:02 +0200

    Fix clean_text and add unit tests

--- python-lib/preprocessing_utils.py
+++ python-lib/preprocessing_utils.py
@@ -8,11 +8,11 @@ import six
 RE_HASHTAG = u"#[a-zA-Z0-9_]+"
 RE_MENTION = u"@[a-zA-Z0-9_]+"
 
-RE_URL = r"(?:https?://|www\.)(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
-RE_EMAIL = r"\b[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+\b"
+RE_URL = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+RE_EMAIL = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""  # noqa
 
 
-TOKENS_TO_IGNORE = [RE_HASHTAG, RE_MENTION, RE_URL, RE_EMAIL]
+TOKENS_TO_IGNORE = [RE_HASHTAG, RE_EMAIL, RE_MENTION, RE_URL]
 
 
 def clean_text(text):
@@ -33,18 +33,18 @@ def clean_text(text):
 
     # ignore urls, mails, twitter mentions and hashtags
     for regex in TOKENS_TO_IGNORE:
-        text = re.sub(regex, " ", text)
+        text = re.sub(regex, "", text)
 
-    text = re.sub(r"<[^>]*>", " ", text)  # remove HTML tags if any
+    text = re.sub(r"<[^>]*>", "", text)  # remove HTML tags if any
 
     # remove the character [']
-    text = re.sub(r"\'", "", text)
+    text = re.sub(r"\'", " ", text)
 
     # this is the default cleaning in Keras,
     # it consists in lowering the texts and removing the punctuation
-    filters = '!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n'
+    filters = u'!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n'
 
-    split = " "  # character that will be used to split the texts later
+    split = u" "  # character that will be used to split the texts later
 
     if isinstance(text, six.text_type):
         translate_map = dict((ord(c), six.u(split)) for c in filters)
@@ -58,4 +58,5 @@ def clean_text(text):
         for c in filters:
             text = text.replace(c, split)
 
-    return text
+    cleaned_text = " ".join(text.split()).strip()
+    return cleaned_text
--- tests/python/unit/test_clean_text.py
+++ tests/python/unit/test_clean_text.py
@@ -5,6 +5,21 @@
 
 from preprocessing_utils import clean_text
 
+# - lowers the string
+# - removes URLs, e-mail adresses
+# - removes Twitter mentions and hastags
+# - removes HTML tags
+# - removes the character [']
+# - replaces punctuation with spaces
 
-def test_dummy_function():
-    assert True
+
+def test_clean_web_text():
+    original_text = u"<p>#THOUGHT envoie dictée à bernard@pivot.fr ou @bernard https://dictee.fr </p>"
+    cleaned_text = clean_text(original_text)
+    assert cleaned_text == u"envoie dictée à ou"
+
+
+def test_clean_punctuation():
+    original_text = u"""Bachibouzouk d'Oh !"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n"""
+    cleaned_text = clean_text(original_text)
+    assert cleaned_text == u"bachibouzouk d oh"
